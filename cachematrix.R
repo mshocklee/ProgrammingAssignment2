e43684a5f3cac6d26ec348b2209333b64c8da074
mshocklee <mshocklee@users.noreply.github.com>
2015-02-22 15:15:25 -0600

    Uncommented makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		inv <- NULL
+		set <- function(y) {
+				x <<- y
+				inv <<- NULL
+		}
+		get <- function() x
+		setinv <- function(inverse) inv <<- inverse
+		getinv <- function() inv
+		list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
