97c7ace97ddd438c290e13d2655b38b2c2517d89
mshocklee <mshocklee@users.noreply.github.com>
2015-02-22 17:00:03 -0600

    Tab reformatting in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -8,15 +8,15 @@
 ## inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-		inv <- NULL
-		set <- function(y) {
-				x <<- y
-				inv <<- NULL
-		}
-		get <- function() x
-		setinv <- function(inverse) inv <<- inverse
-		getinv <- function() inv
-		list(set = set, get = get, setinv = setinv, getinv = getinv)
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inverse) inv <<- inverse
+	getinv <- function() inv
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -25,13 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cache, and return the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-        inv <- x$getinv()
-        if(!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setinv(inv)
-        inv  ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinv()
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setinv(inv)
+	inv  ## Return a matrix that is the inverse of 'x'
 }
